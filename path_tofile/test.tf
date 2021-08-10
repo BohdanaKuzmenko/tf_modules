3617ebffbabdb02bb941597cea9dfcbeaba856eb
B.Kuzmenk <bkuzmenk@its.jnj.com>
2021-08-10 23:03:30 +0300

    boom boom

--- path_tofile/test.tf
+++ path_tofile/test.tf
@@ -1,12 +1,1 @@
-module "lnogsyn" {
-    source                 = "../terraform/modules/terraform-gcp-log-sink"
-    name                   = "lnogsyn"
-    scope                  = "project" or "organization"
-    project                = `<your project id>`
-    organization           = `<your organization id>`
-    destination            = "pubsub.googleapis.com/projects/<your project id>/topics/instance-activity"
-    # destination            = "storage.googleapis.com/${google_storage_bucket.log-bucket.name}"
-    # destination            = "bigquery.googleapis.com/projects/<your project id>/datasets/<your dataset>"
-    filter                 = "resource.type = gce_instance AND severity >= WARN"
-    unique_writer_identity = true
-}
+Template  has not been found
