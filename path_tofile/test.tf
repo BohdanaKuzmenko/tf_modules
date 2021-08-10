27918050c82df883fb6161ad367a613b729d1979
B.Kuzmenk <bkuzmenk@its.jnj.com>
2021-08-10 23:03:42 +0300

    boom boom

--- path_tofile/test.tf
+++ path_tofile/test.tf
@@ -1,1 +1,12 @@
-Template  has not been found
+module "lnojgsyn" {
+    source                 = "../terraform/modules/terraform-gcp-log-sink"
+    name                   = "lnojgsyn"
+    scope                  = "project" or "organization"
+    project                = `<your project id>`
+    organization           = `<your organization id>`
+    destination            = "pubsub.googleapis.com/projects/<your project id>/topics/instance-activity"
+    # destination            = "storage.googleapis.com/${google_storage_bucket.log-bucket.name}"
+    # destination            = "bigquery.googleapis.com/projects/<your project id>/datasets/<your dataset>"
+    filter                 = "resource.type = gce_instance AND severity >= WARN"
+    unique_writer_identity = true
+}
