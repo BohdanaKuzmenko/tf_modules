48f9c5f217a574b6e7288b0c97a6e009add26671
B.Kuzmenk <bkuzmenk@its.jnj.com>
2021-09-09 16:53:29 +0300

    blabla blabla

--- path_tofile/test.tf
+++ path_tofile/test.tf
@@ -1,12 +1,1 @@
-module "logsik2" {
-    source                 = "../terraform/modules/terraform-gcp-log-sink"
-    name                   = "logsik2"
-    scope                  = "project" or "organization"
-    project                = `<your project id>`
-    organization           = `<your organization id>`
-    destination            = "pubsub.googleapis.com/projects/<your project id>/topics/instance-activity"
-    # destination            = "storage.googleapis.com/${google_storage_bucket.log-bucket.name}"
-    # destination            = "bigquery.googleapis.com/projects/<your project id>/datasets/<your dataset>"
-    filter                 = "resource.type = gce_instance AND severity >= WARN"
-    unique_writer_identity = true
-}
+Error processing request: unexpected char '\\' at 830
